6ac5471311fc534b5c293bc658347019edef04c3
mrenoon <vunguyenvyvp@gmail.com>
2017-08-29 15:37:45 +0800

    Fix tests for ACDates

--- test/ACDates.js
+++ test/ACDates.js
@@ -8,29 +8,27 @@ contract('ACDates', function () {
   before(async function () {
     mockACDates = await MockACDates.new();
   });
-
+  const now = function () {
+    return Math.floor(Date.now() / 1000);
+  };
   describe('if_before(timestamp)', function () {
-    const now = Math.floor(Date.now() / 1000); // get timestamp in seconds
-    const pastTimestamp = bN(now - 1);
-    const futureTimestamp = bN(now + 5);
-
     it('throws when timestamp is in the past', async function () {
+      const pastTimestamp = bN(now() - 1);
       assert.ok(await a.failure(mockACDates.test_if_before.call(pastTimestamp)));
     });
     it('does not throw when timestamp is in the future', async function () {
+      const futureTimestamp = bN(now() + 1);
       assert.equal(await mockACDates.test_if_before.call(futureTimestamp), true);
     });
   });
 
   describe('if_after(timestamp)', function () {
-    const now = Math.floor(Date.now() / 1000); // get timestamp in seconds
-    const pastTimestamp = bN(now - 1);
-    const futureTimestamp = bN(now + 1);
-
     it('throws when timestamp is in the future', async function () {
+      const futureTimestamp = bN(now() + 1);
       assert.ok(await a.failure(mockACDates.test_if_after.call(futureTimestamp)));
     });
     it('does not throw when timestamp is in the past', async function () {
+      const pastTimestamp = bN(now() - 1);
       assert.equal(await mockACDates.test_if_after.call(pastTimestamp), true);
     });
   });
