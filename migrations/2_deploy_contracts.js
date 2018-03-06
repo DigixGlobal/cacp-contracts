83490a1232d1d035d23678c4bb4adb5ccee05fbe
Rohit Narurkar <rohitnarurkar@gmail.com>
2018-03-06 14:58:24 +0800

    ACOwnedTest removed

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,10 +1,8 @@
-const ACOwnedTest = artifacts.require('./ACOwnedTest.sol');
 const ContractResolver = artifacts.require('./ContractResolver.sol');
 const MockResolverClient = artifacts.require('./MockResolverClient.sol');
 
-module.exports = function(deployer, network, accounts) {
+module.exports = function (deployer, network, accounts) {
   deployer.deploy(ContractResolver).then(() => {
-    deployer.deploy(ACOwnedTest);
     return deployer.deploy(MockResolverClient, ContractResolver.address);
   })
 };
